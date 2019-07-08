ad76741bf5ddf6ca8ba4b49ac63e5b18ab78ed76
Bikin Maharjan <bikinmaharjan10@gmail.com>
2019-07-09 02:14:09 +1000

    Vue router working

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,13 +8,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
   <title>Owlf Dashboard</title>
 
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+<div class="wrapper" id="app">
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
@@ -67,10 +68,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-             <a href="#" class="nav-link">
+             <router-link to="/dashboard" class="nav-link">
                <i class="nav-icon fas fa-tachometer-alt"></i>
                <p>Dashboard</p>
-             </a>
+             </router-link>
           </li>
 
           <li class="nav-item has-treeview menu-open">
@@ -97,10 +98,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </ul>
           </li>
           <li class="nav-item">
-             <a href="#" class="nav-link">
+             <router-link to="/profile" class="nav-link">
                <i class="nav-icon fas fa-user"></i>
                <p>Profile</p>
-             </a>
+             </router-link>
           </li>
           <li class="nav-item">
              <a href="#" class="nav-link">
